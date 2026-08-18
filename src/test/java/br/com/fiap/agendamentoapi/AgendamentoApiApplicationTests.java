8f96c7517b3f90527204295ed34f1bbee70c1c55
ByGustavoo <132701636+ByGustavoo@users.noreply.github.com>
2026-08-18 20:49:08 -0300

    ADJT-1
    - Commit de Inicialização

---
+++ src/test/java/br/com/fiap/agendamentoapi/AgendamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.agendamentoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
